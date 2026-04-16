96aa9aa50c5de8f2d572e86f9370fd9a82ba7268
Dreamwreaper <48769000+Dreamwreaper@users.noreply.github.com>
2026-04-16 10:21:42 -0400

    Update access-review.ps1

--- automation/access-review.ps1
+++ automation/access-review.ps1
@@ -1,1 +1,6 @@
+Connect-MgGraph
 
+$user = Get-MgUser -Filter "userPrincipalName eq 'test.user@yourtenant.onmicrosoft.com'"
+$group = Get-MgGroup -Filter "displayName eq 'Employees'"
+
+New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $user.Id
