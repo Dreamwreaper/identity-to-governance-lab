d41e6e54ef63f8cfe0ed8b4d711cea0c9958ee5b
Dreamwreaper <48769000+Dreamwreaper@users.noreply.github.com>
2026-04-16 10:21:18 -0400

    Add script to create a new user in Microsoft Graph

--- automation/create-users.ps1
+++ automation/create-users.ps1
@@ -1,1 +1,7 @@
+Connect-MgGraph -Scopes "User.ReadWrite.All"
 
+New-MgUser -DisplayName "Test User" `
+-UserPrincipalName "test.user@yourtenant.onmicrosoft.com" `
+-AccountEnabled $true `
+-MailNickname "testuser" `
+-PasswordProfile @{Password="TempP@ss123!";ForceChangePasswordNextSignIn=$true}
