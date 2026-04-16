0c7a644e98c69c2a7797f1b86d8f895aa7245daf
Dreamwreaper <48769000+Dreamwreaper@users.noreply.github.com>
2026-04-16 10:22:03 -0400

    Add script to export group members to CSV
    Fetch all groups and their members, then export to CSV.

--- automation/assign-groups.ps1
+++ automation/assign-groups.ps1
@@ -1,1 +1,17 @@
+Connect-MgGraph
 
+$groups = Get-MgGroup -All
+$report = @()
+
+foreach ($group in $groups) {
+    $members = Get-MgGroupMember -GroupId $group.Id -All
+    foreach ($member in $members) {
+        $user = Get-MgUser -UserId $member.Id
+        $report += [PSCustomObject]@{
+            Group = $group.DisplayName
+            User  = $user.UserPrincipalName
+        }
+    }
+}
+
+$report | Export-Csv ".\access-review.csv" -NoTypeInformation
